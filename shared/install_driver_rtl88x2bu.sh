deccc532c859312794e7ce446270e1f1e68b9fd9
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2023-12-15 18:43:15 -0500

    add install_driver_rtl88x2bu

---
+++ shared/install_driver_rtl88x2bu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Install Linux Driver for USB WiFi Adapters that are based on the RTL8812BU
+# and RTL8822BU Chipsets
+#
+
+set -e
+
+sudo apt install -y \
+  linux-headers-"$(uname -r)" \
+  build-essential \
+  bc \
+  dkms \
+  git \
+  libelf-dev \
+  rfkill \
+  iw
+
+(
+  cd "$HOME/Downloads"
+  git clone https://github.com/morrownr/88x2bu-20210702.git
+  cd 88x2bu-20210702
+  sudo ./install-driver.sh
+  cd ..
+  rm -rf 88x2bu-20210702
+)
